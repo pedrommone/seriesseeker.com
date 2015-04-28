e8e1c6cb4642d19ad4ca082c377f760f7ee1cc41
Pedro maia <pedro@pedromm.com>
2015-04-27 21:18:13 -0300

    Removed useless comment

--- app/models/User.php
+++ app/models/User.php
@@ -9,19 +9,9 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 
 	use UserTrait, RemindableTrait;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
 	protected $table = 'users';
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
+	protected $hidden = ['password', 'remember_token'];
 
 	public function movies() {
 
