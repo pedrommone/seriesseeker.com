72e068bd73a88b9095785622bf4dd6d15d189662
Pedro Maia <pedromaia@dialhost.com.br>
2015-05-15 10:14:40 -0300

    Added users layout

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -1,21 +1,90 @@
-<section id="shows-statuses" class="container">
-			
-	<div class="row">
+@extends('master')
 
-		<div class="col-md-6">
-			
-			<div class="well">
+@section('content')
+	
+	@include('partials.global-search')
+	
+	<section id="shows-statuses" class="container">
+				
+		<div class="row">
 
-				<h4>Entrar</h4>
+			<div class="col-md-6">
+				
+				<div class="well">
+
+					<form class="form-horizontal">
+						<fieldset>
+							<legend>Entrar</legend>
+
+							<div class="form-group">
+								<label for="email" class="col-lg-2 control-label">Email</label>
+								<div class="col-lg-10">
+									<input type="email" class="form-control" id="email" placeholder="Email">
+								</div>
+							</div>
+
+							<div class="form-group">
+								<label for="password" class="col-lg-2 control-label">Senha</label>
+								<div class="col-lg-10">
+									<input type="password" class="form-control" id="password" placeholder="Senha">
+								</div>
+							</div>
+
+							<div class="form-group">
+								<div class="col-lg-10 col-lg-offset-2">
+									<button type="submit" class="btn btn-primary">Entrar</button>
+								</div>
+							</div>
+						</fieldset>
+					</form>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				
+				<div class="well">
+
+					<form class="form-horizontal">
+						<fieldset>
+							<legend>Criar conta</legend>
+
+							<div class="form-group">
+								<label for="name" class="col-lg-2 control-label">Nome</label>
+								<div class="col-lg-10">
+									<input type="name" class="form-control" id="name" placeholder="Nome">
+								</div>
+							</div>
+
+							<div class="form-group">
+								<label for="email" class="col-lg-2 control-label">Email</label>
+								<div class="col-lg-10">
+									<input type="email" class="form-control" id="email" placeholder="Email">
+								</div>
+							</div>
+
+							<div class="form-group">
+								<label for="password" class="col-lg-2 control-label">Senha</label>
+								<div class="col-lg-10">
+									<input type="password" class="form-control" id="password" placeholder="Senha">
+								</div>
+							</div>
+
+							<div class="form-group">
+								<label for="timezone" class="col-lg-2 control-label">Fuso horário</label>
+								<div class="col-lg-10">
+									{{ Form::select('timezone', $timezones, null, ['class' => 'form-control']); }}
+								</div>
+							</div>
+
+							<div class="form-group">
+								<div class="col-lg-10 col-lg-offset-2">
+									<button type="submit" class="btn btn-primary">Entrar</button>
+								</div>
+							</div>
+						</fieldset>
+					</form>
+				</div>
 			</div>
 		</div>
-
-		<div class="col-md-6">
-			
-			<div class="well">
-
-				<h4>Criar conta</h4>
-			</div>
-		</div>
-	</div>
-</section>
+	</section>
+@stop
