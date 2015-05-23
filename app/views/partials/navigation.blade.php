53e1b12b67a424cfb19ed79db42224256a07250b
Pedro maia <pedro@pedromm.com>
2015-05-23 11:36:22 -0300

    Added active class

--- app/views/partials/navigation.blade.php
+++ app/views/partials/navigation.blade.php
@@ -1,12 +1,21 @@
-<li class="active"><a href="{{ url('/') }}">Home</a></li>
+<li {{ Request::segment(1) == '' ? 'class="active"' : '' }}><a href="{{ url('/') }}">Home</a></li>
 
 @if (Auth::check())
 	
-	<li><a href="{{ url('/my-account') }}">Minha conta</a></li>
-	<li><a href="{{ url('/users/logout') }}">Sair</a></li>
+	<li{{ Request::segment(1) == 'my-account' ? ' class="active"' : ''}}>
+		<a href="{{ url('/my-account') }}">Minha conta</a>
+	</li>
+	
+	<li>
+		<a href="{{ url('/users/logout') }}">Sair</a>
+	</li>
 @else
 
-	<li><a href="{{ url('/users') }}">Criar conta/Entrar</a></li>
+	<li{{ Request::segment(1) == 'users' ? ' class="active"' : '' }}>
+		<a href="{{ url('/users') }}">Criar conta/Entrar</a>
+	</li>
 @endif
 
-<li><a href="{{ url('/status') }}">Status</a></li>
+<li{{ Request::segment(1) == 'status' ? ' class="active"' : '' }}>
+	<a href="{{ url('/status') }}">Status</a>
+</li>
