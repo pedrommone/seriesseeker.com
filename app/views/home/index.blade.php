d86ff0cdb934660cb948015331d409ce7eb5e479
Pedro maia <pedro@pedromm.com>
2015-06-02 20:58:11 -0300

    Added features

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -3,6 +3,30 @@
 @section('content')
 
 	@include('partials.alerts-box')
+
+	<section id="home" class="container">
+		
+		<div class="row text-center">
+
+			<div class="col-md-4">
+				
+				<img src="{{ url('img/iconmonstr-checkbox-2-icon-256.png') }}" alt="">
+				<h4>Acompanhe as suas séries e filmes com uma interface simples</h4>
+			</div>
+
+			<div class="col-md-4">
+				
+				<img src="{{ url('img/iconmonstr-video-5-icon-256.png') }}" alt="">
+				<h4>Descubra um mundo de filmes e séries com a nossa busca</h4>
+			</div>
+
+			<div class="col-md-4">
+				
+				<img src="{{ url('img/iconmonstr-megaphone-7-icon-256.png') }}" alt="">
+				<h4>Seja notificado e receba relatórios sobre seus gostos</h4>
+			</div>
+		</div>
+	</section>
 	
 	@include('partials.global-search')
 	
