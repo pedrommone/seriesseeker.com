e6ab554a8d39e1a2e48686e1f76dc8a15aad18a6
marciomansur <mrabeloo@gmail.com>
2015-06-02 22:21:03 -0300

    Fixin' nubage

--- app/routes.php
+++ app/routes.php
@@ -23,3 +23,8 @@ Route::controller('season-episodes', 'SeasonEpisodesController');
 Route::controller('seasons', 'SeasonsController');
 Route::controller('my-account', 'MyAccountController');
 Route::controller('shows', 'ShowsController');
+
+Route::get('teste', function(){
+
+	
+});
