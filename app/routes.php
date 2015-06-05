aaed3c89e1b74eb223a3314b633b0563e5334938
Pedro Maia <pedromaia@dialhost.com.br>
2015-06-05 09:19:18 -0300

    Cannot be here

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,5 @@
 <?php
 
-use Tmdb\Laravel\Facades\Tmdb;
-
 Route::get('/', 'HomeController@getIndex');
 
 Route::controller('status', 'StatusController');
@@ -13,10 +11,3 @@ Route::controller('seasons', 'SeasonsController');
 Route::controller('my-account', 'MyAccountController');
 Route::controller('calendar', 'CalendarController');
 Route::controller('shows', 'ShowsController');
-
-Route::get('teste', function(){
-
-	$movie = TMDB::getMoviesApi()->getMovie(151590);
-
-	return var_dump($movie);
-});
