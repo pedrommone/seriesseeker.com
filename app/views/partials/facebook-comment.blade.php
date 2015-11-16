dc9d2effa93a6253b2dd97e597323760eee06b5c
Pedro Maia <pedromaia@dialhost.com.br>
2015-11-16 15:13:53 -0200

    Fixed social code

--- app/views/partials/facebook-comment.blade.php
+++ app/views/partials/facebook-comment.blade.php
@@ -3,10 +3,10 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=1654610448161259";
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5&appId=914007821993008";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
 <section id="facebook-comment" class="container">
-	<div class="fb-comments" data-href="{{ Request::url() }}" data-numposts="20"></div>
+	<div class="fb-comments" data-href="http://developers.facebook.com/docs/plugins/comments/" data-width="100%" data-numposts="5"></div>
 </section>
